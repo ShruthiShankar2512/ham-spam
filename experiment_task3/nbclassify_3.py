10733d5b0a5a07482376ec5fb6f88244ee295540
ShruthiShankar2512 <65683215+ShruthiShankar2512@users.noreply.github.com>
2020-05-20 13:59:46 -0700

    Trained after some Feature Modifications

---
+++ experiment_task3/nbclassify_3.py
@@ -0,0 +1,194 @@
+import sys 
+import os 
+import string
+import json
+from nltk.corpus import stopwords 
+from nltk.stem import PorterStemmer
+import re
+from timeit import default_timer as timer
+from autocorrect import Speller
+
+from timeit import default_timer as timer
+import math
+
+def read_from_json(file_path):
+    with open(file_path) as json_file:
+        data = json.load(json_file)
+        return data
+
+def process_json(json_data):
+    return set(json_data["vocabulary"]), json_data["prior_probabilities"], json_data["conditional_probabilities"]
+
+
+def read_data_test(data_path):
+    text_dict = {}
+    #walk through all the directories and subdirectories in the training directory
+    for dirpath, dirs, files in os.walk(data_path):
+        for filename in files:
+            if filename.endswith(".txt"):
+                #count+=1
+                fname = os.path.join(dirpath,filename)
+                #open the file, read it and store it in the the data array
+                try:
+                    with open(fname, encoding = "latin1") as f:
+                        text_dict[fname]=[f.read()]
+                except:
+                    print("Couldnt read ", fname)
+                    continue
+
+    return text_dict
+
+
+
+#removes punctuation from the text 
+def remove_new_line(text):
+    mystring = text.replace('\n', ' ').replace('\r', '')
+    return mystring
+
+def remove_punctuation(text):
+    translator = str.maketrans("", "", string.punctuation)
+    return text.translate(translator)
+
+#calls remove_punctuation to remove punctuation and ten tokenizes the text
+def tokenize(text):
+    text = remove_new_line(text).lower()
+    text = remove_punctuation(text)
+    return text.split()
+
+
+def remove_stop_words(tokenized_text):
+    stop_words = set(stopwords.words('english')) 
+    filtered_sentence = [w for w in tokenized_text if not w in stop_words] 
+    return filtered_sentence
+
+
+def remove_numeric_words(text):
+    re.sub(r'\w*\d\w*', '', text).strip()
+    return text
+
+
+def correct_spelling(tokenized_text):
+    spell = Speller(lang='en')
+    corrected_text = [spell(w) for w in tokenized_text]
+    return corrected_text
+
+
+ 
+#cleans data by removing punctuation and tokenizing the text.
+def get_clean_data(text_list):
+    ps = PorterStemmer()
+    clean_data = []
+    for t in text_list:
+        t = remove_numeric_words(t)
+        tokenized_text = tokenize(t)
+        no_stop_words_text = remove_stop_words(tokenized_text)
+
+        stemmed_text = [ps.stem(w) for w in no_stop_words_text]
+        clean_data.append(stemmed_text)
+    return clean_data
+
+
+def apply_NB_model(text, vocabulary, conditional_probabilities, pp_spam, pp_ham, spam_length, ham_length, vocab_length):
+    #calculate spam score
+    prob_score_spam = pp_spam
+    prob_score_ham = pp_ham
+
+    
+
+    #for each work in the email
+    for token in text:
+        #if the token is not in the vocabulary, skip it.
+        if token in vocabulary:
+            try:
+                prob_score_spam += math.log( conditional_probabilities[0][token] )
+                #print("\n\n\n\n\n\n\nupdated spam score", prob_score_spam)
+            except KeyError:
+                prob_score_spam += math.log((0+1)/( spam_length + vocab_length ))
+        
+            try:
+                prob_score_ham += math.log(conditional_probabilities[1][token] )
+                #print("\n\n\n\n\n\n\nupdated spam score", prob_score_spam)
+            except KeyError:
+                prob_score_ham += math.log((0+1)/( ham_length + vocab_length ))
+        else:
+            #print(token)
+            #print("token not in vocab")
+            continue
+    label = "null"
+    if prob_score_spam >= prob_score_ham:
+        label = "spam"
+    else:
+        label = "ham"
+    return label
+    
+
+
+
+
+def NB_classify(data_dict, vocabulary, prior_probabilities, conditional_probabilities):
+    for k in data_dict.keys():   
+        clean_data = get_clean_data(data_dict[k])
+        data_dict[k] = clean_data.pop()
+    
+    output_list = []
+    #for each email
+
+    spam_length = conditional_probabilities[0]["total_spam_word_count"]
+    vocab_length = len(vocabulary)
+    ham_length = conditional_probabilities[1]["total_ham_word_count"]
+
+    pp_spam = math.log(prior_probabilities["spam"])
+    pp_ham = math.log(prior_probabilities["ham"])
+
+    pp_spam = prior_probabilities["spam"]
+    pp_ham = prior_probabilities["ham"]
+    count = 0
+    for k in data_dict.keys():
+    	count+=1
+    	email = data_dict.get(k)
+    	label = apply_NB_model(email, vocabulary, conditional_probabilities, pp_spam, pp_ham, spam_length, ham_length, vocab_length)
+
+    	output_list.append(''.join([label,"\t",k]))
+    	#if count%100 == 0:
+    		#print(count, label, k)
+    
+    with open("nboutput.txt", "w") as outfile:
+        outfile.write("\n".join(output_list))
+        
+
+
+def main():
+	path_to_data = sys.argv[1]
+
+	start_reading = timer()
+	data_dict = read_data_test(path_to_data)
+	finish_reading = timer()
+	print("time to read", finish_reading-start_reading)
+	print("read test data")
+
+	#get the NB model from the stored text file.
+	vocabulary, PP, CP = process_json(read_from_json("nbmodel.txt"))
+	print("read nb model")
+
+	#classify the test data
+	start_classifying = timer()
+	NB_classify(data_dict, vocabulary, PP, CP)
+	end_classifying = timer()
+	print("time to classify all data: ", end_classifying - start_classifying)
+	print("classification done")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+            
+            
+            
+        
---
+++ experiment_task3/nblearn_3.py
@@ -0,0 +1,253 @@
+import sys 
+import os 
+import string
+import json
+from nltk.corpus import stopwords 
+from nltk.stem import PorterStemmer
+import re
+from timeit import default_timer as timer
+from autocorrect import Speller
+
+
+
+#function to read the data from the text files. 
+def read_data(data_path):
+    text = []
+    target = []
+    
+    spam_ham_dict = {"spam": [], "ham": []}
+    
+    #walk through all the directories and subdirectories in the training directory
+    for dirpath, dirs, files in os.walk(data_path):
+        for filename in files:
+            fname = os.path.join(dirpath,filename)
+            
+            #open the file, read it and store it in the respective list in the dictionary, 
+            #find the right dictionary key (label) by checking whether it is in the ham or spam directory. 
+            with open(fname, "r", encoding = "latin1") as f:
+                if dirpath[-3:] == "ham":
+                	try:
+                		spam_ham_dict["ham"].append(f.read())
+                	except:
+                		print("couldnt read ", fname)
+                		continue
+                elif dirpath[-4:] == "spam":
+                	try:
+                		spam_ham_dict["spam"].append(f.read())
+                	except:
+                		print("couldnt read ", fname)
+                		continue
+                else:
+                	continue    
+    return spam_ham_dict
+
+
+
+
+
+
+#function to read the data from the text files. 
+def read_data_10percent(data_path):
+    text = []
+    target = []
+    
+    spam_ham_dict = {"spam": [], "ham": []}
+    spam_count = 0
+    ham_count = 0
+    
+    #walk through all the directories and subdirectories in the training directory
+    for dirpath, dirs, files in os.walk(data_path):
+        count = 0
+        for filename in files:
+            count+=1
+            fname = os.path.join(dirpath,filename)
+            
+            #open the file, read it and store it in the respective list in the dictionary, 
+            #find the right dictionary key (label) by checking whether it is in the ham or spam directory. 
+            with open(fname, encoding = "latin1") as f:
+                if dirpath[-3:] == "ham":
+                    ham_count+=1
+                    if ham_count > 950:
+                        #if both spam and ham are greater than the required numbers, break
+                        if spam_count > 750:
+                            return spam_ham_dict
+                        else:
+                            continue
+                    else:
+                        spam_ham_dict["ham"].append(f.read())
+
+                elif dirpath[-4:] == "spam":
+                    spam_count+=1
+                    if spam_count > 750:
+                        if ham_count > 950:
+                            return spam_ham_dict
+                        else:
+                            continue
+                    else:
+                        spam_ham_dict["spam"].append(f.read())
+                else:
+                    continue
+    return spam_ham_dict
+
+
+
+#removes punctuation from the text 
+def remove_new_line(text):
+    mystring = text.replace('\n', ' ').replace('\r', '')
+    return mystring
+
+def remove_punctuation(text):
+    translator = str.maketrans("", "", string.punctuation)
+    return text.translate(translator)
+
+#calls remove_punctuation to remove punctuation and ten tokenizes the text
+def tokenize(text):
+    text = remove_new_line(text).lower()
+    text = remove_punctuation(text)
+    return text.split()
+
+
+def remove_stop_words(tokenized_text):
+    stop_words = set(stopwords.words('english')) 
+    filtered_sentence = [w for w in tokenized_text if not w in stop_words] 
+    return filtered_sentence
+
+
+def remove_numeric_words(text):
+    re.sub(r'\w*\d\w*', '', text).strip()
+    return text
+
+
+def correct_spelling(tokenized_text):
+    spell = Speller(lang='en')
+    corrected_text = [spell(w) for w in tokenized_text]
+    return corrected_text
+
+
+ 
+#cleans data by removing punctuation and tokenizing the text.
+def get_clean_data(text_list):
+    ps = PorterStemmer()
+    clean_data = []
+    for t in text_list:
+        t = remove_numeric_words(t)
+        tokenized_text = tokenize(t)
+        no_stop_words_text = remove_stop_words(tokenized_text)
+
+        stemmed_text = [ps.stem(w) for w in no_stop_words_text]
+        clean_data.append(stemmed_text)
+    return clean_data
+
+#gets all the words from te sentence list
+def get_words(sentence_list):
+    word_list = [item for sublist in sentence_list for item in sublist]
+    return word_list
+
+#gets a set of vocabulary from the word list.
+def get_vocabulary(word_list):
+    return set(word_list)
+
+
+def get_all_word_counts(word_list):
+    word_count_dict = {}
+    for word in word_list:
+        if word in word_count_dict:
+            word_count_dict[word] += 1
+        else:
+            word_count_dict[word] = 1
+    return word_count_dict
+
+
+
+
+
+
+
+
+#takes the data dictionary that is read from the files, computes the vocabulary, prior probabilities and the conditional probabilities and returns them.
+def train_NB_model(spam_ham_dict):
+    
+    #clean the data
+    for k in spam_ham_dict.keys():
+        clean_data = get_clean_data(spam_ham_dict[k])
+        spam_ham_dict[k] = clean_data
+        
+    prior_probabilities = {"spam":0, "ham":0}
+
+    
+    #get the counts and a prior probabilities
+    spam_count = len(spam_ham_dict["spam"])
+    ham_count = len(spam_ham_dict["ham"])
+    total_count = spam_count + ham_count
+    
+    prior_probabilities["spam"] = spam_count/total_count
+    prior_probabilities["ham"] = ham_count/total_count
+    
+    
+    #get all the words.
+    spam_words = get_words(spam_ham_dict["spam"])
+    ham_words = get_words(spam_ham_dict["ham"])
+    spam_words_total_count = len(spam_words)
+    ham_words_total_count = len(ham_words)
+    all_words = spam_words + ham_words
+
+    vocabulary = list(get_vocabulary(all_words))
+    
+    
+    #get the word counts
+    spam_word_count = get_all_word_counts(spam_words)
+    ham_word_count = get_all_word_counts(ham_words)
+    
+    
+    #get all the conditional probabilities.
+    spam_conditional_probabilities = {k: v/spam_count for k,v in spam_word_count.items()}
+    ham_conditional_probabilities = {k: v/ham_count for k,v in ham_word_count.items()}
+    
+    #get all the laplace smoothed conditional probabilities
+    spam_conditional_probabilities_smoothed = {k: (v+1)/(spam_words_total_count+len(vocabulary)) for k,v in spam_word_count.items()}
+    ham_conditional_probabilities_smoothed = {k: (v+1)/(ham_words_total_count+len(vocabulary)) for k,v in ham_word_count.items()}
+
+    spam_conditional_probabilities_smoothed["total_spam_word_count"] = spam_words_total_count
+    ham_conditional_probabilities_smoothed["total_ham_word_count"] = ham_words_total_count
+    
+    
+    #conditional probabilities of spam and ham for every token
+    conditional_probabilites = [spam_conditional_probabilities, ham_conditional_probabilities]
+    smoothed_conditional_probabilities = [spam_conditional_probabilities_smoothed, ham_conditional_probabilities_smoothed]
+    
+    return vocabulary, prior_probabilities, smoothed_conditional_probabilities
+    
+    
+
+
+
+def main():
+	path_to_data = sys.argv[1]
+
+	
+	start_reading = timer()
+	data_dict = read_data(path_to_data)
+	finish_reading = timer()
+	print("time to read", finish_reading-start_reading)
+
+
+	print(len(data_dict["spam"]))
+	print(len(data_dict["ham"]))
+
+	start_training = timer()
+	V,PP,CP = train_NB_model(data_dict)
+	end_training = timer()
+	print("time to train ", end_training - start_training)
+	print("trained model")
+
+	model_data = {"vocabulary": V, "prior_probabilities": PP, "conditional_probabilities":CP}
+	json.dump(model_data, open("nbmodel.txt",'w', encoding = "latin1"))
+	print("json dump done")
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+
